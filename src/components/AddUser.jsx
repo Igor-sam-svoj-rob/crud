0793d66d5f80e54a0ff8fe29419286f70f793926
Igor-sam-svoj-rob <ijevremovic@yahoo.com>
2022-08-08 12:59:53 +0200

    crud2

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import TextInput from "./TextInput";
+import Button from "./Button";
 
 const AddUser = () => {
+  const [values, setValues] = useState({
+    ime: "",
+    email: "",
+  });
+
+  const handleAddUser = () => {
+    console.log(values);
+  };
+
   return (
     <div className="mt-10 max-w-xl mx-auto">
       <TextInput
         label="Ime"
+        value={values.ime}
+        onChange={(e) => setValues({ ...values, ime: e.target.value })}
         inputProps={{ type: "text", placeholder: "Ime" }}
       />
+      <TextInput
+        label="E-mail"
+        value={values.email}
+        onChange={(e) => setValues({ ...values, email: e.target.value })}
+        inputProps={{ type: "text", placeholder: "E-mail" }}
+      />
+      <Button onClick={handleAddUser} />
     </div>
   );
 };
